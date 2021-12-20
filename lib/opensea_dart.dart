0ebb51f87757b5e14534770519144c81edc58f60
johnrowan <copypasteearth@gmail.com>
2021-12-20 12:14:27 -0500

    got rid of some warnings on variable names, removed print statements

--- lib/opensea_dart.dart
+++ lib/opensea_dart.dart
@@ -11,7 +11,7 @@ class OpenSea {
 
   };
   OpenSea(String? apiKey){
-    if(apiKey != null && !apiKey.isEmpty){
+    if(apiKey != null && apiKey.isNotEmpty){
       headers["X-API-KEY"] = apiKey;
     }
 
@@ -19,42 +19,39 @@ class OpenSea {
 
   Future<CollectionObject> getCollection(String collection)async{
 
-
     var url = Uri.parse('https://api.opensea.io/api/v1/collection/$collection');
     var response = await http.get(url,headers: headers);
-    //print('Response status: ${response.statusCode}');
-    //print('Response body: ${response.body}');
 
     return CollectionObject.fromJson(jsonDecode(response.body));
   }
 
-  Future<AssetsObject> getAssets(String? owner, List<String>? tokenIds, String? asset_contract_address, List<String>? asset_contract_addresses, String? order_by, String? order_direction, String? offset, String? limit, String? collection)async{
+  Future<AssetsObject> getAssets(String? owner, List<String>? tokenIds, String? assetContractAddress, List<String>? assetContractAddresses, String? orderBy, String? orderDirection, String? offset, String? limit, String? collection)async{
     var queryParameters = {"token_ids":[],"asset_contract_addresses":[],"limit":"20","offset":"0"};
-  if(owner != null && !owner.isEmpty){
+  if(owner != null && owner.isNotEmpty){
     queryParameters["owner"] = owner;
   }
-  if(order_by != null && !order_by.isEmpty){
-    queryParameters["order_by"] = order_by;
+  if(orderBy != null && orderBy.isNotEmpty){
+    queryParameters["order_by"] = orderBy;
   }
-  if(order_direction != null && !order_direction.isEmpty){
-    queryParameters["order_direction"] = order_direction;
+  if(orderDirection != null && orderDirection.isNotEmpty){
+    queryParameters["order_direction"] = orderDirection;
   }
-  if(offset != null && !offset.isEmpty){
+  if(offset != null && offset.isNotEmpty){
     queryParameters["offset"] = offset;
   }
-  if(limit != null && !limit.isEmpty){
+  if(limit != null && limit.isNotEmpty){
     queryParameters["limit"] = limit;
   }
   if(tokenIds != null){
     queryParameters["token_ids"] = tokenIds;
   }
-  if(asset_contract_address != null){
-    queryParameters["asset_contract_address"] = asset_contract_address;
+  if(assetContractAddress != null){
+    queryParameters["asset_contract_address"] = assetContractAddress;
   }
-  if(asset_contract_addresses != null){
-    queryParameters["asset_contract_addresses"] = asset_contract_addresses;
+  if(assetContractAddresses != null){
+    queryParameters["asset_contract_addresses"] = assetContractAddresses;
   }
-  if(collection != null && !collection.isEmpty){
+  if(collection != null && collection.isNotEmpty){
     queryParameters["collection"] = collection;
   }
     var uri = Uri(
@@ -64,12 +61,8 @@ class OpenSea {
       fragment: '',
       queryParameters: queryParameters,
     );
-  print(uri);
 
-    //var url = Uri.https('https://api.opensea.io',"/api/v1/assets",queryParameters);
     var response = await http.get(uri,headers: headers);
-    print('Response status: ${response.statusCode}');
-    //print('Response body: ${response.body}');
 
     return AssetsObject.fromJson(jsonDecode(response.body));
   }
